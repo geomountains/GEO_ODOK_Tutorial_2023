7a5b911cd1c6dfca8f85c5588ab8ba75b0cc259b
geomountains <83764736+geomountains@users.noreply.github.com>
2023-06-15 00:35:31 +0200

    Update init_db.sh
    Adding: 
    
    sudo su postgres
    createdb template_postgis
    
    psql -d template_postgis -c "CREATE EXTENSION postgis;"
    psql -d template_postgis -c "CREATE EXTENSION postgis_topology;"
    psql -d template_postgis -c "CREATE EXTENSION postgis_raster;"

--- init_db/postgres/init_db.sh
+++ init_db/postgres/init_db.sh
@@ -6,7 +6,14 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #Demo PostgreSQL Database initialisation
 psql postgres -c "CREATE USER postgres PASSWORD 'Postgres1'"
 #The -O flag below sets the user: createdb -O DBUSER DBNAME
+
+sudo su postgres
+createdb template_postgis
+
+psql -d template_postgis -c "CREATE EXTENSION postgis;"
+psql -d template_postgis -c "CREATE EXTENSION postgis_topology;"
+psql -d template_postgis -c "CREATE EXTENSION postgis_raster;"
+
 createdb -T template_postgis -O postgres tutorial
-psql tutorial -c "create extension postgis_raster"
 
 psql -d tutorial -U postgres -f $THISDIR/seed_db.sql
