cc2f876b403ebe492dae8d17654da5b62f8e1d16
geomountains <83764736+geomountains@users.noreply.github.com>
2023-06-15 00:27:20 +0200

    Update init_db.sh
    Adding:
    
    -T template_postgis
    psql tutorial -c "create extension postgis_raster"

--- init_db/postgres/init_db.sh
+++ init_db/postgres/init_db.sh
@@ -6,6 +6,7 @@ THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #Demo PostgreSQL Database initialisation
 psql postgres -c "CREATE USER postgres PASSWORD 'Postgres1'"
 #The -O flag below sets the user: createdb -O DBUSER DBNAME
-createdb -O postgres tutorial
+createdb -T template_postgis -O postgres tutorial
+psql tutorial -c "create extension postgis_raster"
 
 psql -d tutorial -U postgres -f $THISDIR/seed_db.sql
