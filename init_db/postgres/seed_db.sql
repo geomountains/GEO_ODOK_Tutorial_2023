96b48ad2eea669cbbb760436d4ccc7c300201ca1
geomountains <83764736+geomountains@users.noreply.github.com>
2023-06-14 16:06:15 +0200

    Update seed_db.sql

--- init_db/postgres/seed_db.sql
+++ init_db/postgres/seed_db.sql
@@ -4,3 +4,6 @@ DROP TABLE IF EXISTS quickdemo CASCADE;
 CREATE TABLE quickdemo(id INT, name VARCHAR(20), value INT);
 INSERT INTO quickdemo VALUES(1,'This',12);
 INSERT INTO quickdemo VALUES(2,'That',345);
+
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_raster;
