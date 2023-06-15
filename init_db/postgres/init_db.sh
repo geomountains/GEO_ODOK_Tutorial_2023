c19bd9d48b0558e878beb91dc9bc1000948969f0
geomountains <83764736+geomountains@users.noreply.github.com>
2023-06-15 17:50:12 +0200

    Update init_db.sh
    Added -w (no password)

--- init_db/postgres/init_db.sh
+++ init_db/postgres/init_db.sh
@@ -13,6 +13,6 @@ psql -d template_postgis -c "CREATE EXTENSION postgis;"
 psql -d template_postgis -c "CREATE EXTENSION postgis_topology;"
 psql -d template_postgis -c "CREATE EXTENSION postgis_raster;"
 
-createdb -T template_postgis -O postgres tutorial
+createdb -w -T template_postgis -O postgres tutorial
 
 psql -d tutorial -U postgres -f $THISDIR/seed_db.sql
